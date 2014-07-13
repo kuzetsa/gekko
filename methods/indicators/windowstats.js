482a410f78110c5198e29eb3c0ea8dd5c826b2d9
Sarah White <kuzetsa@yurizoku.tk>
2014-07-13 16:00:05 -0400

    Binary search for faster window stats
    "in-place" AKA "incremental sorting"
    
    Old code wasted time to do a full sort:
    
    http://en.wikipedia.org/wiki/Quicksort
    full sort is, on average: O(n log n)
    with a worst case of "quadratic time"
    
    http://en.wikipedia.org/wiki/Binary_search_algorithm
    the new incremental sort is O(log n)
    AKA logarithmic time, so much faster.
    
    algorithm coded 2014 June 13 -- kuzetsa

--- methods/indicators/windowstats.js
+++ methods/indicators/windowstats.js
@@ -4,7 +4,14 @@
 // 5/10/25/40/50/60/75/90/95th
 // the "50th" percentile is the median.
 
+var BinarySearch = require('./BinarySearch.js');
+
 var Indicator = function(period) {
+  this.period = period;
+  this.window = [];
+  this.sorted = [];
+  this.enough = false;
+  this.age = 0;
   this.p5th = 0;
   this.p10th = 0;
   this.p25th = 0;
@@ -14,50 +21,59 @@ var Indicator = function(period) {
   this.p75th = 0;
   this.p90th = 0;
   this.p95th = 0;
-
-  this.period = period;
-  this.window = [];
-  this.enough = false;
-  this.age = 0;
+  this.p5ndx = Math.floor(this.period * (5 / 100));
+  this.p10ndx = Math.floor(this.period * (10 / 100));
+  this.p25ndx = Math.floor(this.period * (25 / 100));
+  this.p40ndx = Math.floor(this.period * (40 / 100));
+  this.p50ndx = Math.floor(this.period * (50 / 100));
+  this.p60ndx = Math.floor(this.period * (60 / 100));
+  this.p75ndx = Math.floor(this.period * (75 / 100));
+  this.p90ndx = Math.floor(this.period * (90 / 100));
+  this.p95ndx = Math.floor(this.period * (95 / 100));
 }
 
 Indicator.prototype.update = function(price) {
+  var oldest = 0;
+  var index = 0;
+
   if (this.age >= this.period) {
     this.enough = true;
   }
+
   if (this.enough) {
-  this.window.splice(0,1); // remove first (oldest) from moving window
+    oldest = this.window[0];
+    // remove first (oldest) from...
+
+    // incremental sorting array:
+    index = BinarySearch(this.window, oldest);
+    this.sorted.splice(this.sorted, 1); // remove only
+
+    // moving window:
+    this.window.splice(0, 1);
   }
 
   this.age++;
+
   this.window.push(price);
+
+  index = BinarySearch(this.window, price);
+  this.sorted.splice(index, 0, price) // insert only
+
   this.calculate(price);
 
   return this.p50th; // return the median as a default
 }
 
 Indicator.prototype.calculate = function() {
-  var p5th = Math.floor(this.period * (5 / 100));
-  var p10th = Math.floor(this.period * (10 / 100));
-  var p25th = Math.floor(this.period * (25 / 100));
-  var p40th = Math.floor(this.period * (40 / 100));
-  var p50th = Math.floor(this.period * (50 / 100));
-  var p60th = Math.floor(this.period * (60 / 100));
-  var p75th = Math.floor(this.period * (75 / 100));
-  var p90th = Math.floor(this.period * (90 / 100));
-  var p95th = Math.floor(this.period * (95 / 100));
-
-  var sorted = this.window;
-  sorted.sort()
-  this.p5th = sorted[p5th];
-  this.p10th = sorted[p10th];
-  this.p25th = sorted[p25th];
-  this.p40th = sorted[p40th];
-  this.p50th = sorted[p50th];
-  this.p60th = sorted[p60th];
-  this.p75th = sorted[p75th];
-  this.p90th = sorted[p90th];
-  this.p95th = sorted[p95th];
+  this.p5th = this.sorted[this.p5ndx];
+  this.p10th = this.sorted[this.p10ndx];
+  this.p25th = this.sorted[this.p25ndx];
+  this.p40th = this.sorted[this.p40ndx];
+  this.p50th = this.sorted[this.p50ndx];
+  this.p60th = this.sorted[this.p60ndx];
+  this.p75th = this.sorted[this.p75ndx];
+  this.p90th = this.sorted[this.p90ndx];
+  this.p95th = this.sorted[this.p95ndx];
 }
 
 module.exports = Indicator;

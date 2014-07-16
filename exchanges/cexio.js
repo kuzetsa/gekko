7fcc0b44c1f4ec2284acc9263b59050efbab0bfe
Sarah White <kuzetsa@yurizoku.tk>
2014-07-16 13:19:25 -0400

    cex.io API wrapper un-re-deobfucscation Also, fixed the error handler (hopefully)

--- exchanges/cexio.js
+++ exchanges/cexio.js
@@ -22,24 +22,26 @@ var Trader = function(config) {
   );
 
   _.bindAll(this);
-}
+};
 
 Trader.prototype.getTrades = function(since, callback, descending) {
   var args = _.toArray(arguments);
   var process = function(err, trades) {
-    if(err || !trades || trades.length === 0)
+    if (err || !trades || trades.length === 0) {
       return this.retry(this.getTrades, args, err);
+    }
 
-    var f = parseFloat;
+    var f = parseFloat; // Why is this even here? -- kuzetsa 2014 July 16
 
-    if(descending)
+    if (descending) {
       callback(null, trades);
-    else
+    } else {
       callback(null, trades.reverse());
-  }
+    }
+  };
 
   this.cexio.trades({}, _.bind(process, this));
-}
+};
 
 Trader.prototype.buy = function(amount, price, callback) {
   // Prevent "You incorrectly entered one of fields."
@@ -51,17 +53,20 @@ Trader.prototype.buy = function(amount, price, callback) {
   log.debug('BUY', amount, this.asset, '@', price, this.currency);
 
   var set = function(err, data) {
-    if(err)
+    if (err) {
       return log.error('unable to buy:', err);
-    if(data.error)
+    }
+
+    if (data.error) {
       return log.error('unable to buy:', data.error);
+    }
 
     log.debug('BUY order placed.  Order ID', data.id);
     callback(null, data.id);
   };
 
   this.cexio.place_order('buy', amount, price, _.bind(set, this));
-}
+};
 
 Trader.prototype.sell = function(amount, price, callback) {
   // Prevent "You incorrectly entered one of fields."
@@ -76,17 +81,20 @@ Trader.prototype.sell = function(amount, price, callback) {
   log.debug('SELL', amount, this.asset, '@', price, this.currency);
 
   var set = function(err, data) {
-    if(err)
+    if (err) {
       return log.error('unable to sell:', err);
-    if(data.error)
+    }
+
+    if (data.error) {
       return log.error('unable to sell:', data.error);
+    }
 
     log.debug('SELL order placed.  Order ID', data.id);
     callback(null, data.id);
   };
 
   this.cexio.place_order('sell', amount, price, _.bind(set, this));
-}
+};
 
 Trader.prototype.retry = function(method, args, err) {
   var wait = +moment.duration(10, 'seconds');
@@ -102,117 +110,134 @@ Trader.prototype.retry = function(method, args, err) {
   // make sure the callback (and any other fn)
   // is bound to Trader
   _.each(args, function(arg, i) {
-    if(_.isFunction(arg))
+    if (_.isFunction(arg)) {
       args[i] = _.bind(arg, self);
+    }
   });
 
   // run the failed method again with the same
   // arguments after wait
   setTimeout(
-    function() { method.apply(self, args) },
+    function () { method.apply(self, args); },
     wait
   );
-}
+};
 
 Trader.prototype.getPortfolio = function(callback) {
   var args = _.toArray(arguments);
-  var calculate = function(err, data) {
-    if(err)
+
+  // BEGIN FUNCTION DEFINITION (from inside a different function)
+  var calculate = function (err, data) {
+    if (err) {
       return this.retry(this.getPortfolio, args, err);
+    }
 
     // error handler to troubleshoot issue #4
-    var itBroke = false;
-    if (typeof data == "object") {
-    log.debug("API data object returned from getPortfolio()");
+    var itBroke = false; // assume it's NOT broken until proven otherwise
+
+    if (typeof data === "object") {
+      // exchange sent back a data object
+      log.debug("API data object returned from getPortfolio()");
+
+      if (typeof data.BTC === "object") {
+        // exchange included a BTC object within data object 
+        log.debug("data.BTC object returned from getPortfolio()");
+      } else if (typeof data.BTC === "undefined") {
+        // exchange DID NOT include a valid BTC object
+        itBroke = true;
+        log.debug("data.BTC undefined state during getPortfolio()");
+      } else {
+        itBroke = true;
+        log.debug("strange API data for BTC, neither object nor undefined during getPortfolio()");
+      }
+
+      if (typeof data.NMC === "object") {
+        // exchange included a NMC object within data object 
+        log.debug("data.NMC object returned from getPortfolio()");
+      } else if (typeof data.NMC === "undefined") {
+        // exchange DID NOT include a valid NMC object
+        itBroke = true;
+        log.debug("data.NMC undefined state during getPortfolio()");
+      } else {
+        itBroke = true;
+        log.debug("strange API data for NMC, neither object nor undefined during getPortfolio()");
+      }
+
+      if (typeof data.GHS === "object") {
+        // exchange included a GHS object within data object 
+        log.debug("data.GHS object returned from getPortfolio()");
+      } else if (typeof data.GHS === "undefined") {
+        // exchange DID NOT include a valid GHS object
+        itBroke = true;
+        log.debug("data.GHS undefined state during getPortfolio()");
+      } else {
+        itBroke = true;
+        log.debug("strange API data for GHS, neither object nor undefined during getPortfolio()");
+      }
+
+    } else if (typeof data === "undefined") {
+      log.debug("API data undefined state during getPortfolio()");
+      itBroke = true;
+    } else {
+      log.debug("strange API data, neither object nor undefined during getPortfolio()");
+      itBroke = true;
     }
-    if (typeof data.BTC == "object") {
-    log.debug("data.BTC object returned from getPortfolio()");
+
+    if (itBroke) {
+      log.debug("itBroke");
+      return;
     }
-    if (typeof data.GHS == "object") {
-    log.debug("data.GHS object returned from getPortfolio()");
+
+    if (typeof data.BTC.available === "undefined") {
+      log.debug("data.BTC.available undefined state during getPortfolio()");
     }
-    if (typeof data.NMC == "object") {
-    log.debug("data.NMC object returned from getPortfolio()");
+    if (typeof data.GHS.available === "undefined") {
+      log.debug("data.GHS.available undefined state during getPortfolio()");
     }
-    if (typeof data == "undefined") {
-    itBroke = true;
-    log.debug("API data undefined state during getPortfolio()");
+    if (typeof data.NMC.available === "undefined") {
+      log.debug("data.NMC.available undefined state during getPortfolio()");
     }
-    if (typeof data.BTC == "undefined") {
-    itBroke = true;
-    log.debug("data.BTC undefined state during getPortfolio()");
+    if (typeof data.BTC.orders === "undefined") {
+      log.debug("data.BTC.orders undefined state during getPortfolio()");
     }
-    if (typeof data.GHS == "undefined") {
-    itBroke = true;
-    log.debug("data.GHS undefined state during getPortfolio()");
+    if (typeof data.GHS.orders === "undefined") {
+      log.debug("data.GHS.orders undefined state during getPortfolio()");
     }
-    if (typeof data.NMC == "undefined") {
-    itBroke = true;
-    log.debug("data.NMC undefined state during getPortfolio()");
+    if (typeof data.NMC.orders === "undefined") {
+      log.debug("data.NMC.orders undefined state during getPortfolio()");
     }
-    if (typeof data.BTC.available == "undefined") {
-    itBroke = true;
-    log.debug("data.BTC.available undefined state during getPortfolio()");
-    }
-    if (typeof data.GHS.available == "undefined") {
-    itBroke = true;
-    log.debug("data.GHS.available undefined state during getPortfolio()");
-    }
-    if (typeof data.NMC.available == "undefined") {
-    itBroke = true;
-    log.debug("data.NMC.available undefined state during getPortfolio()");
-    }
-    if (typeof data.BTC.orders == "undefined") {
-    // itBroke = true;
-    log.debug("data.BTC.orders undefined state during getPortfolio()");
-    }
-    if (typeof data.GHS.orders == "undefined") {
-    // itBroke = true;
-    log.debug("data.GHS.orders undefined state during getPortfolio()");
-    }
-    if (typeof data.NMC.orders == "undefined") {
-    // itBroke = true;
-    log.debug("data.NMC.orders undefined state during getPortfolio()");
-    }
+
     log.debug("data.BTC.available is:", typeof data.BTC.available);
     log.debug("data.BTC.orders is:", typeof data.GHS.orders);
     log.debug("data.GHS.available is:", typeof data.GHS.available);
     log.debug("data.GHS.orders is:", typeof data.GHS.orders);
     log.debug("data.NMC.available is:", typeof data.GHS.available);
     log.debug("data.NMC.orders is:", typeof data.GHS.orders);
+  
+    var currency = parseFloat(data.BTC.available); // FIXME: hardcoded
 
-    if (itBroke) {
-    // the condition where "orders" is undefined...
-    // the parseFloat() javascript utility turns it into zero
-    // that's fortunate, as cex.io only returns "orders" json
-    // when there are active orders on your account :)
-    // -- kuzetsa, 2014 July 6th
-    log.debug("itBroke");
-    return;
-    }
-    // currency & asset handler can be refactored
-    // probably a "switch" statement is less ugly
-    // that will be done in a future commit
-    // this is the end of the error handler section
-    // -- kuzetsa, 2014 July 6th
-
-
-    currency = parseFloat(data.BTC.available)
-    if(parseFloat(data.BTC.orders)){
-      currency -= parseFloat(data.BTC.orders)
-    }
-    assets = parseFloat(data.GHS.available);
-    if( parseFloat(data.GHS.orders)){
-	  assets -= parseFloat(data.GHS.orders);
+    if (parseFloat(data.BTC.orders)) { // FIXME: hardcoded
+      currency -= parseFloat(data.BTC.orders); // FIXME: hardcoded
     }
 
+    var assets = parseFloat(data.GHS.available); // FIXME: hardcoded
+
+    if (parseFloat(data.GHS.orders)) { // FIXME: hardcoded
+      assets -= parseFloat(data.GHS.orders); // FIXME: hardcoded
+    }
+    
     var portfolio = [];
-    portfolio.push({name: 'BTC', amount: currency});
-    portfolio.push({name: 'GHS', amount: assets});
+
+    portfolio.push({name: 'BTC', amount: currency}); // FIXME: hardcoded
+    portfolio.push({name: 'GHS', amount: assets}); // FIXME: hardcoded
+
     callback(err, portfolio);
-  }
+
+
+  }; // END FUNCTION DEFINITION (from inside a different function)
+
   this.cexio.balance(_.bind(calculate, this));
-}
+};
 
 Trader.prototype.getTicker = function(callback) {
   var set = function(err, data) {
@@ -221,45 +246,49 @@ Trader.prototype.getTicker = function(callback) {
       bid: data.bid
     };
     callback(err, ticker);
-  }
+  };
   this.cexio.ticker(_.bind(set, this));
-}
+};
 
 Trader.prototype.getFee = function(callback) {
-  // cexio does currently don't take a fee on trades
+  // cexio fee still hardcoded as of 2014 July 16
   // TODO: isn't there an API call for this?
-  callback(false, 0.002);
-}
+  callback(false, 0.002); // 0.2% is a multiply by 0.002 
+};
 
 Trader.prototype.checkOrder = function(order, callback) {
   var check = function(err, result) {
-
-    if(err)
+    if (err) {
       return callback(false, true);
-    if(result.error)
+    }
+    if(result.error){
       return callback(false, true);
-
+    }
     var exists = false;
+    // Inline function inside another function O_O
     _.each(result, function(entry) {
       if(entry.id === order) {
         exists = true;
         return;
       }
     });
-    callback(err, !exists);
+    callback(err, !exists); // FIXME: syntax is !confusing
   };
 
   this.cexio.open_orders(_.bind(check, this));
-}
+};
 
-Trader.prototype.cancelOrder = function(order) {
-  var check= function(err, result) {
-    if(err)
+Trader.prototype.cancelOrder = function (order) {
+  var check = function (err, result) {
+    if (err) {
       log.error('cancel order failed:', err);
-    if(typeof(result) !== 'undefined' && result.error)
+    }
+    if (typeof(result) !== 'undefined' && result.error) {
       log.error('cancel order failed:', result.error);
-  }
+    }
+  };
+
   this.cexio.cancel_order(order, check);
-}
+};
 
 module.exports = Trader;
